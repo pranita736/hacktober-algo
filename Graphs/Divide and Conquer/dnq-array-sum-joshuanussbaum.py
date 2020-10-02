0cfb1e07762b290b3a758d4b5fa345ac0e92fad5
abhishektandon <35254972+abhishektandon@users.noreply.github.com>
2020-10-02 15:00:47 +0530

    Update dnq-array-sum-joshuanussbaum.py

--- Graphs/Divide and Conquer/dnq-array-sum-joshuanussbaum.py
+++ Graphs/Divide and Conquer/dnq-array-sum-joshuanussbaum.py
@@ -5,6 +5,6 @@ def d_a_c(ar):
 	if len(ar) < 2:
 		return ar[0]
 	# Recursive Steps
-	l_ar = ar[:(len(ar) / 2)]
-	r_ar = ar[(len(ar) / 2):]
+	l_ar = ar[:(len(ar) // 2)]
+	r_ar = ar[(len(ar) // 2):]
 	return (d_a_c(l_ar) + d_a_c(r_ar))
